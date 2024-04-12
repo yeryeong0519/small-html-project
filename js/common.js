29f68be96ac85dcbab1c2b48a92116ad3f8952e9
yeryeong0519 <yeryeong0519@gmail.com>
2024-04-12 10:39:04 +0900

    .

---
+++ js/common.js
@@ -0,0 +1,6 @@
+// commonHead.js
+document.head.innerHTML += `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+`;
